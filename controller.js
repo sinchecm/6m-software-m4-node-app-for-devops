73e38860ccdc916de61df09259c73b99fe2411cf
sinchecm <chechiatming@icloud.com>
2025-08-28 21:11:38 +0800

    fix

--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
-
 // This is the only function that will be called by the endpoint.
-function print(req, res){
-    res.send("Hello world!!");
+function print(req, res) {
+  // const unused = 42;
+  res.send("Hello world! It's a great day!");
 }
 
 module.exports = print;
