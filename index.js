7b755c086b9edbab00a95959eb186493ee50719e
sinchecm <chechiatming@icloud.com>
2025-08-30 16:02:33 +0800

    fix

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-const print = require("./controller");
+// This is parse JSON request bodies
+app.use(express.json());
+
+const { print, getHome, createUser } = require("./controller");
 
 app.get("/", print);
+app.get("/home", getHome);
+app.post("/users", createUser);
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening to port ${process.env.PORT}`);
